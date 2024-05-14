4281d41392939ee8806b6c2008086964789820a1
Prameshwar123 <PrameshwarJakhad@gmail.com>
2024-05-14 18:56:26 +0530

    App bar

--- app/src/main/java/com/example/ebook/components/Components.kt
+++ app/src/main/java/com/example/ebook/components/Components.kt
@@ -1,21 +1,34 @@
 package com.example.ebook.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -24,6 +37,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 @Composable
 fun ReaderLogo(modifier: Modifier = Modifier) {
@@ -115,4 +129,40 @@ fun PasswordVisibility(passwordVisibility: MutableState<Boolean>) {
         Icons.Default.Close
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReaderAppBar(
+    title: String,
+    icon: ImageVector? = null,
+    showProfile: Boolean = true,
+    navController: NavController,
+    onBackArrowClicked:() -> Unit = {}
+) {
+
+    TopAppBar(title = {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            if (showProfile) {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "Logo Icon",
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(12.dp))
+                        .scale(0.9f)
+                )
+
+            }
+            if (icon != null) {
+                Icon(imageVector = icon, contentDescription = "arrow back",
+                    tint = Color.Red.copy(alpha = 0.7f),
+                    modifier = Modifier.clickable { onBackArrowClicked.invoke() })
+            }
+            Spacer(modifier = Modifier.width(40.dp))
+            Text(
+                text = title,
+                color = Color.Red.copy(alpha = 0.7f),
+                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+            )
+        }
+    })}
 
--- app/src/main/java/com/example/ebook/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/example/ebook/screens/home/ReaderHomeScreen.kt
@@ -1,10 +1,55 @@
 package com.example.ebook.screens.home
 
-import androidx.compose.material3.Text
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.ebook.components.ReaderAppBar
+import com.example.ebook.navigation.ReaderScreens
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(navController: NavController) {
-    Text("I'm home")
+    Scaffold(topBar = {
+        ReaderAppBar(title = "EReader", navController = navController)
+
+
+    },
+        floatingActionButton = {
+            FABContent {
+                navController.navigate(ReaderScreens.SearchScreen.name)
+            }
+
+        }) {
+        //content
+        Surface(modifier = Modifier.fillMaxSize()) {
+            //home content
+
+        }
+    }
+
+}
+
+@Composable
+fun FABContent(onTap: () -> Unit) {
+    FloatingActionButton(
+        onClick = { onTap() },
+        shape = RoundedCornerShape(50.dp),
+        containerColor = Color(0xFF92CBDF)
+    ) {
+        Icon(imageVector = Icons.Default.Add, contentDescription = "Add a Book", tint = Color.White)
+
+    }
 }
