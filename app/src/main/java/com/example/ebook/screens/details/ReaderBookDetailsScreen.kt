3b522dbac65807d768529d53b4004908c747955f
Prameshwar123 <PrameshwarJakhad@gmail.com>
2024-05-25 10:54:02 +0530

    detail screen viewmodel

--- app/src/main/java/com/example/ebook/screens/details/ReaderBookDetailsScreen.kt
+++ app/src/main/java/com/example/ebook/screens/details/ReaderBookDetailsScreen.kt
@@ -7,20 +7,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.ebook.components.ReaderAppBar
+import com.example.ebook.data.Resource
+import com.example.ebook.model.Item
 import com.example.ebook.navigation.ReaderScreens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BookDetailsScreen(navController: NavController, bookId: String) {
+fun BookDetailsScreen(navController: NavController, bookId: String, viewModel: DetailsViewModel = hiltViewModel()) {
     Scaffold(topBar = {
         ReaderAppBar(
             title = "Book Details",
@@ -39,7 +44,15 @@ fun BookDetailsScreen(navController: NavController, bookId: String) {
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = "Book id: $bookId")
+                val bookInfo = produceState<Resource<Item>>(initialValue = Resource.Loading()){
+                    value = viewModel.getBookInfo(bookId)
+                }.value
+                if(bookInfo.data == null) {
+                    LinearProgressIndicator()
+                }
+                else{
+                    Text(text = "Book: ${bookInfo.data.volumeInfo.title}")
+                }
             }
         }
     }
